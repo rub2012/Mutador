34401a29f1639933f1f707d1626df93aed32266c
Julian <>
2017-03-21 20:50:21 -0300

    Tarea

--- src/funcion/Funcion.java
+++ src/funcion/Funcion.java
@@ -2,8 +2,10 @@ package funcion;
 
 public class Funcion {
 
+	//Crear metodos para cubrir todos los Casos de Mutacion y que se puedan generar todos los mutantes necesarios
+	//Tambien crear cada mutante en una carpeta distinta
 	public int suma2(int a){
-		return a-2;
+		return a+2;
 	}
 	
 }
