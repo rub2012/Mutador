da10ce6ae7382bef1e09b7390a053a429feb6c97
rub <rub@Xubuntu>
2017-03-21 20:30:36 -0300

    mutado 1

--- src/funcion/Funcion.java
+++ src/funcion/Funcion.java
@@ -3,7 +3,7 @@ package funcion;
 public class Funcion {
 
 	public int suma2(int a){
-		return a+2;
+		return a-2;
 	}
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,31 @@
 package main;
 
+import java.util.List;
+
 import funcion.Funcion;
+import procesadores.aor.ProcesadorAORBresta;
+import spoon.Launcher;
+import spoon.reflect.CtModel;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.visitor.filter.TypeFilter;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Funcion s = new Funcion();
-		System.out.println(s.suma2(2));
+		ProcesadorAORBresta proc = new ProcesadorAORBresta();
+		Launcher launcher = new Launcher();
+		launcher.addInputResource("src/funcion/Funcion.java");
+		TypeFilter<CtBinaryOperator<?>> expresion = new TypeFilter<CtBinaryOperator<?>>(CtBinaryOperator.class);
+		launcher.run();
+		CtModel modelo = launcher.getModel();
+		List<CtBinaryOperator<?>> elementos;
+		elementos = modelo.getElements(expresion);
+		CtBinaryOperator elem = elementos.get(0);
+		proc.process(elem);
+		
+		
+		System.out.println(elem);
+		launcher.prettyprint();
 	}
 
 }
